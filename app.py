ec00c9e13972b6832c9502dbbd273c77564d6373
Naveen kumar Jadi <67469226+naveenkumarkr723@users.noreply.github.com>
2020-09-03 12:40:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask,render_template,url_for,request
+import pandas as pd 
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.externals import joblib
+import pickle
+
+# load the model from disk
+
+clf = joblib.load('foodreview_pkl')
+cv = joblib.load('trans_count_vect')
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        vect = cv.transform(data).toarray()
+        
+        predictions = clf.predict(vect)
+    return render_template('result.html', prediction = predictions)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
